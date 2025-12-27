116246ddb13f39220c2192ba00efe47326ccd791
Harry Weiss <harrisonrweiss1@gmail.com>
2025-12-27 11:09:55 -0500

    fix(brew): update formula to v1.1.6

--- homebrew/intus.rb
+++ homebrew/intus.rb
@@ -1,8 +1,8 @@
 class Intus < Formula
   desc "Local Autonomous Agent and System Sidecar for your terminal"
   homepage "https://github.com/harryw1/intus"
-  url "https://github.com/harryw1/intus/archive/refs/tags/v1.1.4.tar.gz"
-  sha256 "ebb4a3ea1fc78a26dcca77f7dced10b745698cf9ff28fb1ac146124d7726f381"
+  url "https://github.com/harryw1/intus/archive/refs/tags/v1.1.6.tar.gz"
+  sha256 "1fe1de50e55342151c54e5543f98aff597962fd76222390053fee089718cdd60"
   license "MIT"
   head "https://github.com/harryw1/intus.git", branch: "master"
 
